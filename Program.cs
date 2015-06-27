af9c82a3a2b8165e0cefa1d7cd6985be24fd4316
Kevin Erath <KevinErath@users.noreply.github.com>
2015-06-27 14:29:45 +0200

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FromRomanNumerals
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var mapping = new Dictionary<string, int> {{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000}, {"IV", 4}, {"IX", 9}, {"XL", 40}, {"XC", 90}, {"CD", 400}, {"CM", 900}};
+            var number = 0;
+            var lastChar = "-";
+            foreach (var chr in args[0].Select(x => x.ToString()))
+            {
+                if (mapping.ContainsKey(lastChar + chr))
+                {
+                    number += mapping[lastChar + chr] - mapping[lastChar];
+                }
+                else
+                {
+                    number += mapping[chr];
+                }
+                lastChar = chr;
+            }
+            Console.WriteLine("{0} -> {1}", args[0], number);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FromRomanNumerals_FirstSolution
+
+A simple solution which solves the kata 'From roman numerals'.
+
+Read more about this implementation on:
+http://www.code-whisperer.de/2015/03/29/first-solution
